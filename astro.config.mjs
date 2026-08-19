7a26c5faea3fc2d9625fe17ecb0d25f0a09cd4ed
rida-hdj <rida-hdj@proton.me>
2026-08-19 16:15:32 +0100

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rida-hdj.github.io',
+  base: '/the-technical-repo',
+
   markdown: {
     shikiConfig: {
       theme: 'css-variables',
